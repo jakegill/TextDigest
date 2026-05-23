f7bb8c37339fafe855482dfb029ea94c44eab341
Jake Gill <jake.w.g21@gmail.com>
2026-05-23 14:33:29 -0400

    feat(design system): add typefaces and color palette

---
+++ apps/app/src/components/icons/google-icon.tsx
@@ -0,0 +1,30 @@
+export function GoogleIcon({ className }: { className?: string }) {
+	return (
+		<svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="none" viewBox="0 0 48 48" className={className}>
+			<g id="google">
+				<g id="google-vector" fillRule="evenodd" clipRule="evenodd">
+					<path
+						id="Shape"
+						fill="#4285F4"
+						d="M41.28 24.41q-.004-1.914-.328-3.68H24v6.96h9.688a8.28 8.28 0 0 1-3.592 5.434v4.516h5.816c3.404-3.134 5.368-7.748 5.368-13.23"
+					></path>
+					<path
+						id="Shape_2"
+						fill="#34A853"
+						d="M24 42c4.86 0 8.936-1.612 11.914-4.36L30.1 33.12c-1.612 1.08-3.674 1.72-6.096 1.72-4.688 0-8.656-3.168-10.072-7.422H7.916v4.664A18 18 0 0 0 24.002 42"
+					></path>
+					<path
+						id="Shape_3"
+						fill="#FBBC05"
+						d="M13.928 27.424a10.8 10.8 0 0 1-.564-3.42c0-1.186.204-2.34.564-3.42V15.92H7.914A18 18 0 0 0 6 24.004c0 2.904.696 5.654 1.914 8.084z"
+					></path>
+					<path
+						id="Shape_4"
+						fill="#EA4335"
+						d="M24 13.16c2.644 0 5.016.91 6.882 2.692l5.164-5.16C32.926 7.784 28.854 6 24 6a18 18 0 0 0-16.086 9.916l6.014 4.664c1.416-4.254 5.384-7.42 10.072-7.42"
+					></path>
+				</g>
+			</g>
+		</svg>
+	);
+}
